f180de84d9d888fb1953fbd5307790744ab15687
Shiri_xm70@student.itstep.org <Shiri_xm70@student.itstep.org>
2024-12-10 20:45:31 +0200

    Add project files.

---
+++ 1311/1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>, ICloneable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+
+    public int CompareTo(Book other)
+    {
+        return Year.CompareTo(other.Year);
+    }
+
+    public object Clone()
+    {
+        return new Book(Title, Author, Year);
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author}, {Year}";
+    }
+}
+
+public class BookTitleComparer : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        return string.Compare(x.Title, y.Title, StringComparison.OrdinalIgnoreCase);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var books = new List<Book>
+        {
+            new Book("1984", "George Orwell", 1949),
+            new Book("Brave New World", "Aldous Huxley", 1932),
+            new Book("Fahrenheit 451", "Ray Bradbury", 1953)
+        };
+
+        Console.WriteLine("Original list:");
+        books.ForEach(book => Console.WriteLine(book));
+
+        books.Sort();
+        Console.WriteLine("\nSorted by year:");
+        books.ForEach(book => Console.WriteLine(book));
+
+        books.Sort(new BookTitleComparer());
+        Console.WriteLine("\nSorted by title:");
+        books.ForEach(book => Console.WriteLine(book));
+
+        var clone = (Book)books[0].Clone();
+        Console.WriteLine("\nCloned book:");
+        Console.WriteLine(clone);
+    }
+}
---
+++ 1311/2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private List<Book> books;
+
+    public Library()
+    {
+        books = new List<Book>();
+    }
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        foreach (var book in books)
+        {
+            yield return book;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var library = new Library();
+        library.AddBook(new Book("1984", "George Orwell", 1949));
+        library.AddBook(new Book("Brave New World", "Aldous Huxley", 1932));
+        library.AddBook(new Book("Fahrenheit 451", "Ray Bradbury", 1953));
+
+        Console.WriteLine("Library books:");
+        foreach (var book in library)
+        {
+            Console.WriteLine(book);
+        }
+    }
+}
